412ea0ac67b57b9c0b50913569e3dea05f55c99c
Andrzej Galus <redtiger84@users.noreply.github.com>
2022-07-04 01:18:49 +0200

    Example for stream server operation

--- src/main/java/com/grpc/client/CalculatorClient.java
+++ src/main/java/com/grpc/client/CalculatorClient.java
@@ -1,6 +1,7 @@
 package com.grpc.client;
 
 import com.proto.calculator.CalculatorServiceGrpc;
+import com.proto.calculator.PrimeNumberDecompositionRequest;
 import com.proto.calculator.SumRequest;
 import com.proto.calculator.SumResponse;
 import io.grpc.ManagedChannel;
@@ -14,9 +15,24 @@ public class CalculatorClient {
                 .build();
 
         //unary connection
-        calculatorSum(channel);
+        //calculatorSum(channel);
+
+        //server stream
+        calculatorPrimeNumberDecomposition(channel);
 
         channel.shutdown();
+    }
+
+    private static void calculatorPrimeNumberDecomposition(ManagedChannel channel) {
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub stub = CalculatorServiceGrpc.newBlockingStub(channel);
+
+        Integer number = 10000000;
+        stub.primeNumberDecomposition(PrimeNumberDecompositionRequest.newBuilder()
+                        .setNumber(number)
+                        .build())
+                .forEachRemaining(primeNumberDecompositionResponse -> {
+                    System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
+                });
     }
 
     private static void calculatorSum(ManagedChannel channel) {
--- src/main/java/com/grpc/server/CalculatorServiceImpl.java
+++ src/main/java/com/grpc/server/CalculatorServiceImpl.java
@@ -1,8 +1,6 @@
 package com.grpc.server;
 
-import com.proto.calculator.CalculatorServiceGrpc;
-import com.proto.calculator.SumRequest;
-import com.proto.calculator.SumResponse;
+import com.proto.calculator.*;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
@@ -16,6 +14,25 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
 
         responseObserver.onNext(sumResponse);
 
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void primeNumberDecomposition(PrimeNumberDecompositionRequest request, StreamObserver<PrimeNumberDecompositionResponse> responseObserver) {
+
+        Integer number = request.getNumber();
+        Integer divisor = 2;
+
+        while (number > 1) {
+            if (number % divisor == 0) {
+                number /= divisor;
+                responseObserver.onNext(PrimeNumberDecompositionResponse.newBuilder()
+                        .setPrimeFactor(divisor)
+                        .build());
+            } else {
+                ++divisor;
+            }
+        }
         responseObserver.onCompleted();
     }
 }
